38685942ca1756f82477632c5e49f43456a1caa5
maxime <maxime.curon@std.heh.be>
2023-03-24 15:51:00 +0100

    + service getAllUsers()

--- backend/src/main/java/com/cardest/backend/domain/service/UserService.java
+++ backend/src/main/java/com/cardest/backend/domain/service/UserService.java
@@ -1,9 +1,14 @@
 package com.cardest.backend.domain.service;
 
+import com.cardest.backend.adapter.out.jpa.UserJpaEntity;
 import com.cardest.backend.adapter.out.mapper.UserMapper;
 import com.cardest.backend.adapter.out.repository.UserRepository;
+import com.cardest.backend.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,10 @@ public class UserService {
 
     private final UserRepository userRepository;
     private UserMapper userMapper;
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(user -> users.add(userMapper.toDomainEntity(user)));
+        return users;
+    }
 }
