8dbb0a726c9707cb15a39c897bd4a0bae909a96d
maxime <maxime.curon@std.heh.be>
2023-04-12 05:28:26 +0200

    + remove unused imports

--- src/main/java/com/cardest/backend/adapter/in/UserController.java
+++ src/main/java/com/cardest/backend/adapter/in/UserController.java
@@ -4,7 +4,6 @@ import com.cardest.backend.domain.model.User;
 import com.cardest.backend.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
